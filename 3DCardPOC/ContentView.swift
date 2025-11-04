f9524e5474498caf54dc785cad6f76a3b45dc538
meshal <imeshalfm@gmail.com>
2025-11-04 10:29:16 +0300

    Initial Commit

---
+++ 3DCardPOC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  3DCardPOC
+//
+//  Created by meshal on 11/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 3DCardPOC/_DCardPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DCardPOCApp.swift
+//  3DCardPOC
+//
+//  Created by meshal on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct _DCardPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
